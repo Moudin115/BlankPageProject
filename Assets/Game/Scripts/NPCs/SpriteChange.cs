04801ed5f481cabf73cde49721cdf62f78c1892e
Moudin115 <76106118+Moudin115@users.noreply.github.com>
2021-07-05 14:44:49 +0200

    made Player Camera Sprite Script

---
+++ Assets/Game/Scripts/NPCs/SpriteChange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour
+{
+
+    public GameObject camObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Items.Camera == true)
+        {
+            camObj.SetActive(false);
+        }
+    }
+}
