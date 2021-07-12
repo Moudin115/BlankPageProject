d2ea6689210b4cd5ff1d7fa1a9ace3ed16091f92
Moudin115 <76106118+Moudin115@users.noreply.github.com>
2021-07-12 21:17:17 +0200

    added packpack to sprite | added E to escape photowall

--- Assets/Game/Scripts/NPCs/Specified/PackTrig.cs
+++ Assets/Game/Scripts/NPCs/Specified/PackTrig.cs
@@ -6,11 +6,25 @@ public class PackTrig : MonoBehaviour
 {
     public static bool hasPacked;
 
+    public bool inCol;
+
+    private void Update()
+    {
+        if (inCol)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                hasPacked = true;
+                Items.PickUpLuggage();
+                Destroy(gameObject);
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            hasPacked = true;
+            inCol = true;
         }
     }
 }
