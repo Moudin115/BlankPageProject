4b40e27703947041fdf5af8d81d7a422212a33b7
Moudin115 <76106118+Moudin115@users.noreply.github.com>
2021-07-13 16:05:30 +0200

    added leaf, mother anim, backgroundscroll

---
+++ Assets/Game/Scripts/Environment/Background.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector2(player.transform.position.x, 0) ;
+    }
+}
