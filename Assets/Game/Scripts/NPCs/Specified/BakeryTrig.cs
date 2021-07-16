d7bd0bf7c1eeabe536c3967d30a5c27e261b7069
Moudin115 <76106118+Moudin115@users.noreply.github.com>
2021-07-16 18:51:35 +0200

    fixed bakery trigger

---
+++ Assets/Game/Scripts/NPCs/Specified/BakeryTrig.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class BakeryTrig : MonoBehaviour
+{
+    private bool TriggerTrig = false;
+    private DialogueRunner dialogueRunner;
+
+    private void Start()
+    {
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (TriggerTrig == false)
+            {
+                dialogueRunner.StartDialogue("Start");
+                TriggerTrig = true;
+            }
+        }
+    }
+}
